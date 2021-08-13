768f0ee4761312b5f13d94bbbe7363de4e01c065
Rujrawee Komolsai <rujrawee.k@ibm.com>
2021-08-13 11:53:52 +0700

    revert app.module misconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './controller/todo.controller';
 import { TodoService } from './service/todo.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {TodoEntity} from "./repository/todo.entity";
-import {TodoRepository} from "./repository/todo.repository";
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './repository/todo.entity';
+import { TodoRepository } from './repository/todo.repository';
+//mysql.nimbus-bootcamp.svc.cluster.local
+import { TodoModule } from './todo.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'bootcamp',
-      password: 'cyberpunk2077',
-      database: 'bootcamp',
+      host: `${process.env.DB_HOST}` || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: `${process.env.DB_USERNAME}` || `bootcamp`,
+      password: `${process.env.DB_PASSWORD}` || `cyberpunk2077`,
+      database: `${process.env.DB_DATABASE}` || `bootcamp`,
       entities: [TodoEntity],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([TodoEntity])
+    TodoModule,
   ],
+  exports: [TypeOrmModule],
   controllers: [TodoController],
   providers: [TodoService, TodoRepository],
 })
