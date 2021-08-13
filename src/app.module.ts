05eb39d751debc117bda57181ae8631d9c16ce79
Naomi Lin <supakorn.t@ibm.com>
2021-08-13 11:49:52 +0700

    ADD Dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,17 @@ import { TodoEntity } from './repository/todo.entity';
 import { TodoRepository } from './repository/todo.repository';
 //mysql.nimbus-bootcamp.svc.cluster.local
 import { TodoModule } from './todo.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'bootcamp',
-      password: 'cyberpunk2077',
-      database: 'bootcamp',
+      host: `${process.env.DB_HOST}` || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: `${process.env.DB_USERNAME}` || `bootcamp`,
+      password: `${process.env.DB_PASSWORD}` || `cyberpunk2077`,
+      database: `${process.env.DB_DATABASE}` || `bootcamp`,
       entities: [TodoEntity],
       synchronize: true,
     }),
